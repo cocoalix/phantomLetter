f03a90f261156a5879ef0951a4f8c34defb7ea8d
cocoalix <cocoalix@gmail.com>
2022-07-10 18:38:30 +0900

    create model

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,9 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
+        'key',
         'name',
-        'email',
-        'password',
+        'token',
     ];
 
     /**
